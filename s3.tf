ac22ce61344dec1b33c25c519021d91df50e93ea
Daulo <daulofilho@gmail.com>
2021-05-28 08:03:30 -0300

    s3 import

--- s3.tf
+++ s3.tf
@@ -5,3 +5,16 @@ resource "aws_s3_bucket" "this" {
 
   tags = local.s3_tags
 }
+
+resource "aws_s3_bucket" "manual" {
+  bucket = "daulomanual1"
+  tags = {
+    "Name"   = "importado"
+    "Updata" = "27/05/21"
+  }
+}
+
+
+
+
+
