2b42c8da67eecc060c27d907b31bca73eae0c965
Daulo <daulofilho@gmail.com>
2021-06-05 12:56:15 -0300

    remote state s3

--- main.tf
+++ main.tf
@@ -13,4 +13,10 @@ provider "aws" {
   region = var.aws_region
 }
 
-
+terraform {
+  backend "s3" {
+    bucket = "tfstate-566740411665"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
--- s3.tf
+++ s3.tf
@@ -14,4 +14,27 @@ resource "aws_s3_bucket" "manual" {
   }
 }
 
+data "aws_caller_identity" "current" {
+
+}
+
+resource "aws_s3_bucket" "remotestate" {
+  bucket = "tfstate-${data.aws_caller_identity.current.account_id}"
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    "Name"   = "remote state"
+    "UpData" = "06/06/21"
+  }
+
+}
+
+output "output_remote_state" {
+  value = aws_s3_bucket.remotestate.arn
+
+}
+
 
