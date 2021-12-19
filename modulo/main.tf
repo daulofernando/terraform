cf60e0f5468646139f27a186c57fea76aca1fad5
Daulo <daulofilho@gmail.com>
2021-12-19 07:24:23 -0300

    iniciando treinamento de terraform

--- modulo/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "this" {
-  bucket = var.name
-  tags   = var.tags
-  policy = var.policy
-  acl    = var.acl
-
-}
--- terraform.tfvars
+++
@@ -1,4 +0,0 @@
-environment   = "main"
-aws_region    = "us-east-1"
-instance_ami  = "ami-09e67e426f25ce0d7"
-instance_type = "t2.micro"
